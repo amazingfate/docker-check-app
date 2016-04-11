6dce5a9467fd04d987ac6152ff1d3bd89ff9779e
amazingfate <liujianfeng1994@gmail.com>
2016-04-11 13:54:43 +0800

    initial commit

---
+++ checkapp.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+check_app(){
+    app=$2
+    runtime=${1:-10m}
+    mypid=$$
+    $app &
+    apppid=$!
+    echo "My PID=$mypid. App's PID=$apppid"
+    ps -f $apppid
+    #Sleep for the specified time.
+    sleep $runtime
+    kill $apppid || echo "$app not start" >> output-$review_id
+}
+apt-get update
+apt-get -y install python python-apt coreutils python-pycurl
+cd /docker-check-app
+python /docker-check-app/get_sourcelist.py
+rm /etc/apt/sources.list
+mv base.list /etc/apt/sources.list.d/
+mv rpa.list /etc/apt/sources.list.d/
+apt-get update
+apt-get -y dist-upgrade
+while read apps
+do 
+    check_app 10s $apps
+done < app.list
---
+++ curl_back.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cmd_res=$1
+host_api=$2
+review_id=$3
+BUILD_URL=$4
+
+if [[ ${cmd_res} == 0 ]]; then
+if [ -f $PWD/output-$review_id ]
+then
+curl -X POST -H Access-Token:${CHECK_TOKEN} ${host_api}/test_result/${review_id} -d "passed=0&comment=check app not pass.\njob details: <a target=\"_blank\" href=\"${BUILD_URL}/console\">${BUILD_URL}</a> "
+rm $PWD/output-$review_id
+else
+curl -X POST -H Access-Token:${CHECK_TOKEN} ${host_api}/test_result/${review_id} -d "passed=1&comment=check app pass.\njob details: <a target=\"_blank\" href=\"${BUILD_URL}/console\">${BUILD_URL}</a> "
+fi
+else
+	curl -X POST -H Access-Token:${CHECK_TOKEN} ${host_api}/test_result/${review_id} -d "passed=0&comment=check app failed.\njob details: <a target=\"_blank\" href=\"${BUILD_URL}/console\">${BUILD_URL}</a> "
+fi
