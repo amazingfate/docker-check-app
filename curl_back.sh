0c3b2b26895c4c35f6d4b4f95875fe3b3e9b842f
amazingfate <liujianfeng1994@gmail.com>
2016-04-18 17:18:41 +0800

    fix curl script error

--- curl_back.sh
+++ curl_back.sh
@@ -12,7 +12,7 @@ cat $PWD/output-$review_id
 curl -X POST -H Access-Token:${CHECK_TOKEN} ${host_api}/test_result/${review_id} -d "passed=0&comment=check app not pass.\njob details: ${BUILD_URL}console"
 rm $PWD/output-$review_id
 else
-curl -X POST -H Access-Token:${CHECK_TOKEN} ${host_api}/test_result/${review_id} -d "passed=1&comment=check app pass.\njob details: "${BUILD_URL}console"
+curl -X POST -H Access-Token:${CHECK_TOKEN} ${host_api}/test_result/${review_id} -d "passed=1&comment=check app pass.\njob details: ${BUILD_URL}console"
 fi
 else
 	curl -X POST -H Access-Token:${CHECK_TOKEN} ${host_api}/test_result/${review_id} -d "passed=0&comment=check app failed.\njob details: ${BUILD_URL}console"
