baf1e856b5c219b276c9f16e96050055cf7f34b7
amazingfate <liujianfeng1994@gmail.com>
2016-04-19 10:31:57 +0800

    change comment to Chinese

--- curl_back.sh
+++ curl_back.sh
@@ -9,11 +9,11 @@ if [[ ${cmd_res} == 0 ]]; then
 if [ -f $PWD/output-$review_id ]
 then
 cat $PWD/output-$review_id
-curl -X POST -H Access-Token:${CHECK_TOKEN} ${host_api}/test_result/${review_id} -d "passed=0&comment=check app not pass.\njob details: ${BUILD_URL}console"
+curl -X POST -H Access-Token:${CHECK_TOKEN} ${host_api}/test_result/${review_id} -d "passed=0&comment=应用检测未通过.\njob details: ${BUILD_URL}console"
 rm $PWD/output-$review_id
 else
-curl -X POST -H Access-Token:${CHECK_TOKEN} ${host_api}/test_result/${review_id} -d "passed=1&comment=check app pass.\njob details: ${BUILD_URL}console"
+curl -X POST -H Access-Token:${CHECK_TOKEN} ${host_api}/test_result/${review_id} -d "passed=1&comment=应用检测通过.\njob details: ${BUILD_URL}console"
 fi
 else
-	curl -X POST -H Access-Token:${CHECK_TOKEN} ${host_api}/test_result/${review_id} -d "passed=0&comment=check app failed.\njob details: ${BUILD_URL}console"
+	curl -X POST -H Access-Token:${CHECK_TOKEN} ${host_api}/test_result/${review_id} -d "passed=0&comment=应用检测失败.\njob details: ${BUILD_URL}console"
 fi
